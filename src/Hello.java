4ffaf5afa70fd3aa38abed17abbb06d687563fc1
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-06 18:43:48 +0600

    Starting out with Expressions commit

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,12 @@ public class Hello {
 
 
         int myFirstNumber = (10 + 5) + (2 * 10);
-        System.out.println("myFirstNumber is = "+ myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("myTotal Number is = "+ myTotal);
+        //assignment
+        int myLastOne = 1000 - myTotal;
+        System.out.println("myLast Number is = "+myLastOne);
     }
 }
