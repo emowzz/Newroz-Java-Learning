7c5600492cbbeacddd934c26940066496decbffe
Fahim Foysal Emon <fahim.foysal@outlook.com>
2021-06-06 18:32:12 +0600

    Variables commit

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,9 @@ public class Hello {
 
     public static void main(String[] args){
         System.out.println("Hello Fahim");
+
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println("myFirstNumber is = "+ myFirstNumber);
     }
 }
